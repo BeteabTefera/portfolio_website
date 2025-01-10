3a3ad71af3ed1a464cab5076018240c476cc9f68
Beteab Tefera <beteabtefera@yahoo.com>
2025-01-10 16:11:18 -0600

    Update about.tsx

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -7,9 +7,9 @@ export function About() {
   return (
     <ThemeProvider>
       <div className="flex flex-col md:flex-row items-center gap-8 mx-5">
-        <div className="md:w-1/2">
+        {/*<div className="md:w-1/2">
           <img src={profileImage} alt="Profile" className="rounded-full w-64 h-64 object-cover object-top mx-auto" />
-        </div>
+        </div>*}
         <div className="dark:text-gray-300 md:w-1/2">
           <h3 className="dark:text-gray-300 text-2xl font-bold mb-4">
             Hey{<HeartHandshake/>} I&apos;m Beteab.</h3>
