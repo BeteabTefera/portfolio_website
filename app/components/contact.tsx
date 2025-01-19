a0e9e8e565f9b497eae1f24290a28fb798196391
Beteab <beteabtefera@yahoo.com>
2025-01-19 02:32:53 -0600

    update

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -98,6 +98,20 @@ export function Contact() {
           </form>
         </section>
       </div>
+      <div className="max-w-md mx-auto">
+            {/*Here I will put logos for Linkedin, Gmail, Github*/}
+            <div className="flex justify-center gap-4 mb-8">
+                <a href="https://www.linkedin.com/in/beteabtefera/" target="_blank">
+                    <Linkedin size={40}/>
+                </a>
+                <a href="mailto:beteabtefera@gmail.com" target="_blank">
+                    <Mail size = {40}/>
+                </a>
+                <a href="https://github.com/BeteabTefera" target="_blank">
+                    <Github size = {40}/>
+                </a>
+            </div>
+        </div>
       <ToastContainer />
     </div>
   );
